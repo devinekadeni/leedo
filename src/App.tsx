19f5bee10bf1379213cbd1549316f1c8c2e216f6
Devin <devinekadeni@gmail.com>
2020-09-07 01:18:13 +0700

    feat: Add redux-saga configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,14 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import { ThemeProvider } from '@material-ui/core/styles'
 import { Provider } from 'react-redux'
-import { compose } from 'redux'
+
 import store from 'reduxpath/store'
+
 import GlobalStyles from 'styles/GlobalStyles'
 import theme from 'styles/MUI-Theme'
 import RoutingComponent from './Routing'
 import { DialogCtxProvider } from 'context/DialogContext'
 import { AuthCtxProvider } from 'context/AuthContext'
-
-declare global {
-  interface Window {
-    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
-  }
-}
 
 const App: React.FC = () => {
   return (
--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,10 +1,20 @@
-import { createStore, compose } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 
 import rootReducer from './root-reducer'
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose
+  }
+}
+
+const sagaMiddleware = createSagaMiddleware()
+const middlewares = [sagaMiddleware]
 
 const devCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const composeEnchancers = process.env.NODE_ENV !== 'production' ? devCompose : compose
 
-const store = createStore(rootReducer, composeEnchancers())
+const store = createStore(rootReducer, composeEnchancers(applyMiddleware(...middlewares)))
 
 export default store
